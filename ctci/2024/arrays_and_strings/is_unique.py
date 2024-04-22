18d6ca80ed1b370b584213aa8558e9675004edce
yent <tyralyntran@gmail.com>
2024-04-21 22:43:07 -0700

    isunique question

--- ctci/2024/arrays_and_strings/is_unique.py
+++ ctci/2024/arrays_and_strings/is_unique.py
@@ -1,3 +1,21 @@
 # Implement an algorithm to determine if a string has all unique characters
 #
-# what if you can't use additional data structures?
+# What if you can't use additional data structures?
+#
+# input:  "green bay packers"
+# output: False
+#
+# input: "abc"
+# output: Trues
+
+from collections import Counter
+from functools import reduce
+
+def isUnique(s):
+	counts = Counter(s).values()
+	return reduce(lambda a,b: (a and b==1), counts)
+
+print(isUnique("green bay packers"))
+print(isUnique("abcdA"))
+
+
