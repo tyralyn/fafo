4bafffd6e83361e65c6eb91f5bbf556c33c9d1ea
yent <tyralyntran@gmail.com>
2024-04-28 21:44:13 -0700

    additrions to string rotation

--- ctci/2024/arrays_and_strings/string_rotation.py
+++ ctci/2024/arrays_and_strings/string_rotation.py
@@ -1,3 +1,18 @@
-# Assume you have a methog isSubstring, that checks if one work is a substring of another. Given two strings s1 and s2, check if s2 is a rotation of s1 using only one call to isSubstring
+# Assume you have a method isSubstring, that checks if one work is a substring of another. Given two strings s1 and s2, check if s2 is a rotation of s1 using only one call to isSubstring
 #
 # Example: "waterbottle" is a rotation of "erbottlewat"
+# python has an is substring method using 
+#
+# substr is in superstr
+# 
+# so im going to do that
+
+def isStringRotation(s1, s2):
+	if len(s1) != len(s2):
+		return False
+	return(s2 in s1*2)
+
+print(isStringRotation("erbottlewat", "waterbottle"))
+print(isStringRotation("erbottlewat", "erbottlewat"))
+print(isStringRotation("a", ""))
+print(isStringRotation("paper", "planes"))
